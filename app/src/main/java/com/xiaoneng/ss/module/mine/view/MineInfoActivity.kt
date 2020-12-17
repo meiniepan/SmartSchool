c1c325d69e6e543ee0665a933c55b902c15187e2
Burning <meiniepan@163.com>
2020-12-17 18:20:04 +0800

    fix android11选择图片出现的问题

--- app/src/main/java/com/xiaoneng/ss/module/mine/view/MineInfoActivity.kt
+++ app/src/main/java/com/xiaoneng/ss/module/mine/view/MineInfoActivity.kt
@@ -180,6 +180,7 @@ class MineInfoActivity : BaseLifeCycleActivity<AccountViewModel>() {
         PictureSelector.create(this)
             .openGallery(PictureMimeType.ofImage())
             .maxSelectNum(1)
+            .isAndroidQTransform(true)
             .imageEngine(GlideEngine.createGlideEngine())
             .forResult(PictureConfig.CHOOSE_REQUEST)
     }
