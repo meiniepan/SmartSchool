aed5165c11f9aa414e011629bdc5868e09a85323
Burning <meiniepan@163.com>
2020-12-18 12:55:03 +0800

    fix 语音识别

--- app/src/main/java/com/xiaoneng/ss/common/utils/aliSpeech/SpeechTranscriberActivity.java
+++ app/src/main/java/com/xiaoneng/ss/common/utils/aliSpeech/SpeechTranscriberActivity.java
@@ -12,7 +12,6 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
