687ebd5b66f2c126678b75bf1e6a42187d2114b6
Burning <meiniepan@163.com>
2021-04-29 18:38:33 +0800

    fix 工资条详情 项目值为0时隐藏

--- app/src/main/java/com/xiaoneng/ss/module/school/adapter/SalaryDetailAdapter.kt
+++ app/src/main/java/com/xiaoneng/ss/module/school/adapter/SalaryDetailAdapter.kt
@@ -1,6 +1,7 @@
 package com.xiaoneng.ss.module.school.adapter
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -22,11 +23,28 @@ class SalaryDetailAdapter(layoutId: Int, listData: MutableList<String>?) :
     var eData: SalaryDetailBean? = null
     override fun convert(viewHolder: BaseViewHolder?, item: String) {
         viewHolder?.let { holder ->
+            var llOut = holder.getView<View>(R.id.llOut)
             var ivRemark = holder.getView<ImageView>(R.id.ivRemark)
             var llRemark = holder.getView<View>(R.id.llSalaryRemark)
             var tvRemark = holder.getView<TextView>(R.id.tvSalaryRemark)
             var remarkBean: SalaryExpandRemarkBean? =
                 eData?.expand?.remark?.get(holder.adapterPosition)
+
+            //value为0时隐藏
+            var layoutParams: ViewGroup.LayoutParams = llOut.getLayoutParams();
+            llOut.visibility = View.VISIBLE
+            layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            if (remarkBean?.isShow == "0") {
+                if (eData?.expand?.vals?.get(holder.adapterPosition) == "0") {
+                    llOut.visibility = View.GONE
+                    layoutParams.height = 0;
+                    layoutParams.width = 0;
+                }
+            }
+            llOut.setLayoutParams(layoutParams);
+
+
             if (remarkBean?.remark.isNullOrEmpty()) {
                 ivRemark.visibility = View.GONE
                 llRemark.visibility = View.GONE
@@ -41,6 +59,7 @@ class SalaryDetailAdapter(layoutId: Int, listData: MutableList<String>?) :
                     llRemark.visibility = View.GONE
                 }
             }
+
             ivRemark.setOnClickListener {
                 if (remarkBean?.show != true) {
                     ivRemark.setImageResource(R.drawable.ic_up)
