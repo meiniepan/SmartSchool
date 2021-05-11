c3d32196812b26dadcc62bdb889a583fe277d5cd
Burning <meiniepan@163.com>
2021-05-11 15:00:53 +0800

    fix 任务状态页面

--- app/src/main/java/com/xiaoneng/ss/module/school/view/TaskActivity.kt
+++ app/src/main/java/com/xiaoneng/ss/module/school/view/TaskActivity.kt
@@ -13,10 +13,7 @@ import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener
 import com.xiaoneng.ss.R
 import com.xiaoneng.ss.base.view.BaseLifeCycleActivity
 import com.xiaoneng.ss.common.state.UserInfo
-import com.xiaoneng.ss.common.utils.Constant
-import com.xiaoneng.ss.common.utils.RecycleViewDivider
-import com.xiaoneng.ss.common.utils.dp2px
-import com.xiaoneng.ss.common.utils.mStartActivity
+import com.xiaoneng.ss.common.utils.*
 import com.xiaoneng.ss.module.school.adapter.DialogListAdapter
 import com.xiaoneng.ss.module.school.adapter.TaskStatusAdapter
 import com.xiaoneng.ss.module.school.model.TaskDetailBean
@@ -33,6 +30,8 @@ import kotlinx.android.synthetic.main.activity_task.*
 class TaskActivity : BaseLifeCycleActivity<SchoolViewModel>() {
     private var lastId: String? = null
     private var status: String? = null
+    private var stime: String? = null
+    private var etime: String? = null
     private var mType: String? = null//1接收 2发布
     lateinit var mAdapter: TaskStatusAdapter
     var mData = ArrayList<TaskDetailBean>()
@@ -50,23 +49,26 @@ class TaskActivity : BaseLifeCycleActivity<SchoolViewModel>() {
         super.initView()
         //todo 权限 AppInfo.checkRule2("teacher/tasks/add"
         initDialogPublish()
-        
+        initDialogReceive()
+
         titlesP.add("进行中")
         titlesP.add("草稿箱")
         titlesP.add("已关闭")
         statusP.add("1")
         statusP.add("0")
         statusP.add("3")
-        
+
         titlesR.add("未完成")
         titlesR.add("已完成")
         statusR.add("0")
         statusR.add("1")
         initAdapter()
         if (UserInfo.getUserBean().usertype != "1") {
+            mType = "2"
             ivAddTask.visibility = View.VISIBLE
             llTitlePublish.visibility = View.VISIBLE
         } else {
+            mType = "1"
             ivAddTask.visibility = View.GONE
             llTitlePublish.visibility = View.GONE
         }
@@ -75,7 +77,10 @@ class TaskActivity : BaseLifeCycleActivity<SchoolViewModel>() {
             mStartActivity<AddTaskActivity>(this)
         }
         tvPublish.setOnClickListener {
-            mType = "1"
+            mType = "2"
+            tvActionStatus.setOnClickListener {
+                dialogPublish.show()
+            }
             tvPublish.setBackgroundResource(R.drawable.bac_blue_bac)
             tvPublish.setTextColor(resources.getColor(R.color.white))
             tvReceive.setBackgroundResource(R.drawable.bac_blue_line_21)
@@ -83,15 +88,41 @@ class TaskActivity : BaseLifeCycleActivity<SchoolViewModel>() {
             initPublishData()
         }
         tvReceive.setOnClickListener {
-            mType = "2"
+            mType = "1"
+            tvActionStatus.setOnClickListener {
+                dialogReceive.show()
+            }
             tvReceive.setBackgroundResource(R.drawable.bac_blue_bac)
             tvReceive.setTextColor(resources.getColor(R.color.white))
             tvPublish.setBackgroundResource(R.drawable.bac_blue_line_21)
             tvPublish.setTextColor(resources.getColor(R.color.themeColor))
-            initReceiveData()
+            initPublishData()
+        }
+
+        tvActionStatus.setOnClickListener {
+            if (UserInfo.getUserBean().usertype != "1") {
+                dialogPublish.show()
+            } else {
+                dialogReceive.show()
+            }
+        }
+        tvActionTimeS.setOnClickListener {
+            showDateDayPick(tvActionTimeS) {
+                stime = this
+                rvTaskStatus.showLoadingView()
+                doRefresh()
+            }
+        }
+        tvActionTimeE.setOnClickListener {
+            showDateDayPick(tvActionTimeE) {
+                etime = this
+                rvTaskStatus.showLoadingView()
+                doRefresh()
+            }
         }
 
     }
+
     private fun initAdapter() {
         rvTaskStatus.setOnRefreshLoadMoreListener(object : OnRefreshLoadMoreListener {
             override fun onLoadMore(refreshLayout: RefreshLayout) {
@@ -122,27 +153,32 @@ class TaskActivity : BaseLifeCycleActivity<SchoolViewModel>() {
             }
         }
     }
-private fun initPublishData(){
-    
-}
-    private fun initReceiveData(){
 
+    private fun initPublishData() {
+        status = null
+        tvActionStatus.text = "任务状态"
+        stime = null
+        tvActionTimeS.text = "开始时间"
+        etime = null
+        tvActionTimeE.text = "结束时间"
+        doRefresh()
+    }
+
+    override fun initData() {
+        super.initData()
+        doRefresh()
     }
 
     override fun getData() {
         //任务状态0待发布1进行中2完成3关闭
-        if (status == "-1") {
-            mViewModel.getTaskList(lastid = lastId)
-        } else {
-            if (mType == "1") {
-                mViewModel.getTaskList(status = status, lastid = lastId)
-            } else if (mType == "2") {
-                mViewModel.getPublishTaskList(status = status, lastid = lastId)
-            }
+        if (mType == "1") {
+            mViewModel.getTaskList(status = status, lastid = lastId)
+        } else if (mType == "2") {
+            mViewModel.getPublishTaskList(status = status, lastid = lastId)
         }
     }
 
-    
+
     private fun initDialogPublish() {
         // 底部弹出对话框
         dialogPublish =
@@ -170,9 +206,45 @@ private fun initPublishData(){
         }
         dialogAdapter.setOnItemClickListener { adapter, view, position ->
             tvActionStatus.text = titlesP[position]
+            status = statusP[position]
             rvTaskStatus.showLoadingView()
             doRefresh()
             dialogPublish.dismiss()
+        }
+
+    }
+
+    private fun initDialogReceive() {
+        // 底部弹出对话框
+        dialogReceive =
+            Dialog(this, R.style.BottomDialog)
+        val contentView: View =
+            LayoutInflater.from(this).inflate(R.layout.dialog_list, null)
+        dialogReceive.setContentView(contentView)
+        val params = contentView.layoutParams as ViewGroup.MarginLayoutParams
+        params.width =
+            resources.displayMetrics.widthPixels
+        params.bottomMargin = dp2px(this, 0f).toInt()
+        contentView.layoutParams = params
+        dialogReceive.window!!.setGravity(Gravity.BOTTOM)
+        dialogReceive.window!!.setWindowAnimations(R.style.BottomDialog_Animation)
+        var dialogAdapter = DialogListAdapter(R.layout.item_dialog_list, titlesR)
+        var recyclerView = contentView.findViewById<RecyclerView>(R.id.rvDialogList).apply {
+            layoutManager = LinearLayoutManager(this@TaskActivity)
+            addItemDecoration(
+                RecycleViewDivider(
+                    dp2px(this@TaskActivity, 1f).toInt(),
+                    this.resources.getColor(R.color.splitColor)
+                )
+            )
+            adapter = dialogAdapter
+        }
+        dialogAdapter.setOnItemClickListener { adapter, view, position ->
+            tvActionStatus.text = titlesR[position]
+            status = statusR[position]
+            rvTaskStatus.showLoadingView()
+            doRefresh()
+            dialogReceive.dismiss()
         }
 
     }
@@ -184,7 +256,7 @@ private fun initPublishData(){
         rvTaskStatus.setNoMoreData(false)
         getData()
     }
-    
+
     override fun initDataObserver() {
         mViewModel.mTaskListData.observe(this, Observer { response ->
             response?.let {
